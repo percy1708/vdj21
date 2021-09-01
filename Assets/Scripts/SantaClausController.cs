814866815a1bc5e4c71deac901e46365445f2ec9
percy1708 <percy_vla@hotmail.com>
2021-09-01 09:34:30 -0500

    t1

---
+++ Assets/Scripts/SantaClausController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SantaClausController : MonoBehaviour
+{
+    // public properties
+    public float velocityX = 15;
+    public float jumpForce = 40;
+
+    // private components
+    private SpriteRenderer sr;
+    private Rigidbody2D rb;
+    private Animator animator;
+    
+    // private properties
+    
+    // constants
+    private const int ANIMATION_IDLE = 0;
+    private const int ANIMATION_RUN = 1;
+    private const int ANIMATION_SLIDE= 2;
+    private const int ANIMATION_JUMP= 3;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Iniciando Game Object");
+        sr = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(0, rb.velocity.y);
+        changeAnimation(ANIMATION_IDLE); 
+        
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = new Vector2(velocityX, rb.velocity.y); 
+            sr.flipX = false;
+            changeAnimation(ANIMATION_RUN);
+        }
+        
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector2(-velocityX, rb.velocity.y);
+            sr.flipX = true;
+            changeAnimation(ANIMATION_RUN);
+        }
+        
+        if (Input.GetKey(KeyCode.X))
+        {
+            changeAnimation(ANIMATION_SLIDE);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse); // salta
+            changeAnimation(ANIMATION_JUMP); // saltar
+        }
+    }
+    private void changeAnimation(int animation)
+    {
+        animator.SetInteger("Estado", animation);
+    }
+}
